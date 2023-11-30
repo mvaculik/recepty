eb31cb4956e9375b6455fedc872e2689e4fc736d
mvaculik <73494801+mvaculik@users.noreply.github.com>
2023-11-30 22:29:35 +0100

    add file

---
+++ MainActivity.kt
@@ -0,0 +1,16 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, FoodDetailFragment())
+                .commit()
+        }
+    }
+}
